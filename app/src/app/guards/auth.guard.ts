6ea0ecd5f17800194cad67eeb265d7e1ec727a92
Marc-Philippe Huget <mphuget@gmail.com>
2021-03-02 21:41:46 +0100

    Specify the test on auth.guard

--- app/src/app/guards/auth.guard.ts
+++ app/src/app/guards/auth.guard.ts
@@ -1,15 +1,32 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import {Router} from '@angular/router';
+
+import { Plugins } from '@capacitor/core';
+const { Storage } = Plugins;
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+  constructor(private router: Router) {
+
   }
+
+  async canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Promise<boolean> {
+
+      const isLogged = await Storage.get({key: "logged"});      
+      if (isLogged && (isLogged.value === 'true')) {
+        return true;
+      } 
+      else {
+        this.router.navigateByUrl('/', { replaceUrl:true });
+        return false;
+      }
+    }
   
 }
