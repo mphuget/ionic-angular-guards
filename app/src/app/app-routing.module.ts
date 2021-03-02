56aed091ea0ddcc3969852778e90d6edc85ab20d
Marc-Philippe Huget <mphuget@gmail.com>
2021-03-02 21:43:39 +0100

    Add the login page

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     canActivate: [
       AuthGuard
     ]
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   }
 ];
 @NgModule({
